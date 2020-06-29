a4afa0572774a999e2163746463c8207476983b2
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-29 19:45:50 +0200

    Creamos cliente Retrofit

---
+++ app/src/main/java/com/jmonzon/popcorn/retrofit/TheMovieDBClient.kt
@@ -0,0 +1,41 @@
+package com.jmonzon.popcorn.retrofit
+
+import com.jmonzon.popcorn.common.Constantes
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TheMovieDBClient {
+    private val theMovieDBService: TheMovieDBService
+    private val retrofit: Retrofit
+
+    companion object {
+        var instance: TheMovieDBClient? = null
+            get() {
+                if (field == null) {
+                    instance = TheMovieDBClient()
+                }
+                return instance
+            }
+    }
+
+    init {
+        //Includes interceptor
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        okHttpClientBuilder.addInterceptor(TheMovieDBInterceptor())
+
+        val client = okHttpClientBuilder.build()
+
+        //Build retrofit client
+        retrofit = Retrofit.Builder()
+            .baseUrl(Constantes.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        //Instance retrofit service from retrofit´s object
+        theMovieDBService = retrofit.create(TheMovieDBService::class.java)
+    }
+
+    fun getTheMovieDBService() = theMovieDBService
+}

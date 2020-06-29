9855735141596e8f7febf2e169182a3e6c777e1e
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-29 18:45:04 +0200

    Añadimos constantes como la base url o la base url de imagenes

--- app/src/main/java/com/jmonzon/popcorn/common/Constantes.kt
+++ app/src/main/java/com/jmonzon/popcorn/common/Constantes.kt
@@ -1,7 +1,11 @@
 package com.jmonzon.popcorn.common
 
 class Constantes {
-    companion object{
-        public val TMDBAPI_BASE_URL = ""
+    companion object {
+        val TMDBAPI_BASE_URL = ""
+        val API_KEY = "40bd3de9a0cc86442c18ee793d9001d8"
+        val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+        val BASE_URL = "https://api.themoviedb.org/3/"
+
     }
 }
